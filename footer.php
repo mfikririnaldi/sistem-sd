975ad759517d58c753b69658178c970cba87a717
mfikririnaldi <muhammadfikririnaldi@gmail.com>
2024-05-20 09:58:02 +0700

    mengunggah file project kelompok4

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+
+<footer style="border-top: 4px solid #ffffff;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ffffff"><b>SD Negeri 2 Doroampel</b></h3>
+					
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> sdn2doroampel@gmail.com</p>
+				</div>
+				
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #ffffff; padding: 5px; color: #ffffff; text-align: center;">
+			<span>Copyright&copy; SD Negeri 2 Doroampel</span>
+		</div>
+	</footer>
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SD Negeri 2 Doroampel</title>
+    <link rel= "stylesheet" type="text/css" href= "style.css" >
+</head>
+<body>
+    <img src= "images/glewa.jpeg">
+</body>
+
+<?php 
+include 'footer.php';
+?>
+
+</html>
